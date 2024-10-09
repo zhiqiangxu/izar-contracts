eee1abdb3149ed5cf2c20d548fed9bc7e90bd309
alan <652732310@qq.com>
2024-10-09 10:53:12 +0800

    add testModBalance

--- test/Slot.t.sol
+++ test/Slot.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/misc/MintAllERC20.sol";
 
 contract SlotTest is Test {
     uint64 public sequenceNumber;
@@ -28,5 +29,15 @@ contract SlotTest is Test {
         // but uint160 assumes the argument to be left padded
         assertNotEq(address(uint160(uint256(bytes32(bytes20(v))))), v);
         assertEq(address(uint160(uint256(bytes32(bytes20(v))))), address(uint160(1 << 96)));
+    }
+
+    function testModBalance() public {
+        MintAllERC20 erc20 = new MintAllERC20("ERC20_NAME", "ERC20_SYMBOL", 1e30);
+        address account = address(1000);
+        bytes32 mapSlot = bytes32(0); // output of forge inspect
+        bytes32 slot = keccak256(abi.encode(account, mapSlot));
+        uint256 value = 1e10;
+        vm.store(address(erc20), slot, bytes32(value));
+        assertEq(erc20.balanceOf(account), value);
     }
 }
